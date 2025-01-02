e1b0c3f45c9fc3b2a16e6b1d7ba3f9777c6509df
liu <liuvaneshka@gmail.com>
2025-01-02 18:10:39 -0300

    add: task entity

---
+++ ITRockTaskManagemen.Entities/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace ITRockTaskManagemen.Entities
+{
+    public class Tarea
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } 
+        public bool IsCompleted { get; set; } = false; 
+        public DateTime? DueDate { get; set; } 
+    }
+}
---
+++ ITRockTaskManagementAPI/Entities/TaskEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace ITRockTaskManagementAPI.Entities
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Task")]
+    [Index(nameof(Id), IsUnique = true, Name = "UX_Task_Id")]
+    public class TaskEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(255)]
+        public string? Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public DateTime? DueDate { get; set; }
+    }
+}
